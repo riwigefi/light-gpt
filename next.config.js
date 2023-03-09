267bfce6a9b0286f3e7b664ab2973f6dff72888a
wjm <wangjm@hrtps.com>
2023-03-09 23:22:00 +0800

    feat: project init

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
+const path = require('path');
+const sass = require('sass');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+    reactStrictMode: true,
+    sassOptions: {
+        includePaths: [path.join(__dirname, 'styles')],
+        implementation: sass,
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
