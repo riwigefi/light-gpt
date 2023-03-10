54551c414da9ef856398e6b6ce8b4ba0ccff1f03
wjm <wangjm@hrtps.com>
2023-03-10 17:38:52 +0800

    fix: vercel variable get

--- pages/api/get_available_api_key.ts
+++ pages/api/get_available_api_key.ts
@@ -1,11 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-    let apiKey;
-    if (process.env.NODE_ENV === 'production') {
-        apiKey = process.env.VERCEL_API_KEY;
-    } else {
-        apiKey = process.env.API_KEY;
-    }
+    const apiKey = process.env.API_KEY;
     res.status(200).json({ apiKey });
 }
