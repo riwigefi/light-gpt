856d46b471ef7e1d44ece1860abe891b09ad3dbf
wjm <wangjm@hrtps.com>
2023-03-11 18:08:25 +0800

    feat: mobile service error

--- pages/index.tsx
+++ pages/index.tsx
@@ -481,6 +481,10 @@ export default function Home() {
                     </div>
                 )}
 
+                <div className={styles.openAiServiceError}>
+                    {serviceErrorMessage || '测试测试'}
+                </div>
+
                 <div className={styles.action}></div>
                 <div className={styles.middle}>
                     <div className={styles.textareaContainer}>
